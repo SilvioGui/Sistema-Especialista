56d8d82ed284ea7abb91512da782ac2a3d9df006
Silvio Guilherme Queiroz <88034950+SilvioGui@users.noreply.github.com>
2023-01-13 16:42:03 -0300

    Update Inferencia.py

---
+++ Inferencia/Inferencia.py
@@ -0,0 +1,24 @@
+class Inferencia:
+  #busca dos dados
+  def get_data(self):
+  
+  #tratamento dos dados
+  def data_processing(self):
+  
+  #coleta de novos dados
+  def prompt_user(self):
+  
+  #log regras
+  def rules_log(self):
+  
+  #inferencia
+  def inference(self):
+  
+  #inferencia
+  def set_data(self):
+  
+  #log dos resultados
+  def result_log(self):
+  
+  #algoritimo de melhor escolha
+  def (self):
