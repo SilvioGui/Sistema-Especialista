bfc21c610df584ca042a5342308b26b249ab643c
Silvio Guilherme Queiroz <88034950+SilvioGui@users.noreply.github.com>
2023-01-16 13:25:43 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,21 +1,13 @@
 import yaml, os
-import  pymysql.cursors
+
 
 with open(os.path.dirname(os.path.realpath(__file__)) + "/config.yml", 'r') as ymlfile:
     config = yaml.full_load(ymlfile)
     
+
 #metodo para conecão com o banco
-
-
 def getConnection():
-    db = config['database']['database']
-    return pymysql.connect(
-                host=config['database']['host'],
-                user=config['database']['user'],
-                password=config['database']['password'],
-                database=db,
-                charset='utf8mb4',
-                cursorclass=pymysql.cursors.DictCursor)
 
+#metodo para conecão com o log
 def getLogging():
 	
